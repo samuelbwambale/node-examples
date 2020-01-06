77e15d55372ae11e33a3f36ce86d668a8f39f495
samuelbwambale <simplesam63@gmail.com>
2020-01-06 12:00:14 -0600

    add express package to Node

--- index.js
+++ index.js
@@ -1,48 +1,75 @@
+
+// // 1 - Build Node Server without Express
+
+// const http = require('http');
+// const fs = require('fs');
+// const path = require('path');
+
+// const hostname = 'localhost';
+// const port = 3000;
+
+// const server = http.createServer((req, res) => {
+//     console.log('Request for ' + req.url + ' by method ' + req.method);
+  
+//     if (req.method == 'GET') {
+//       let fileUrl;
+//       if (req.url == '/') fileUrl = '/index.html';
+//       else fileUrl = req.url;
+  
+//       let filePath = path.resolve('./public'+fileUrl);
+//       const fileExt = path.extname(filePath);
+//       if (fileExt == '.html') {
+//         fs.exists(filePath, (exists) => {
+//           if (!exists) {
+//             res.statusCode = 404;
+//             res.setHeader('Content-Type', 'text/html');
+//             res.end('<html><body><h1>Error 404: ' + fileUrl + 
+//                         ' not found</h1></body></html>');
+//             return;
+//           }
+//           res.statusCode = 200;
+//           res.setHeader('Content-Type', 'text/html');
+//           fs.createReadStream(filePath).pipe(res);
+//         });
+//       }
+//       else { // if file is not an html file
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end('<html><body><h1>Error 404: ' + fileUrl + 
+//                 ' not a HTML file</h1></body></html>');
+//       }
+//     }
+//     else { // if HTTP method is not a GET
+//         res.statusCode = 404;
+//         res.setHeader('Content-Type', 'text/html');
+//         res.end('<html><body><h1>Error 404: ' + req.method + 
+//                 ' not supported</h1></body></html>');
+//     }
+//   })
+
+// server.listen(port, hostname, () => {
+//   console.log(`Server running at http://${hostname}:${port}/`);
+// });
+
+
+// 1 - Build Node Server with Express framework/package
+const express = require('express');
 const http = require('http');
-const fs = require('fs');
-const path = require('path');
 
 const hostname = 'localhost';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    console.log('Request for ' + req.url + ' by method ' + req.method);
-  
-    if (req.method == 'GET') {
-      let fileUrl;
-      if (req.url == '/') fileUrl = '/index.html';
-      else fileUrl = req.url;
-  
-      let filePath = path.resolve('./public'+fileUrl);
-      const fileExt = path.extname(filePath);
-      if (fileExt == '.html') {
-        fs.exists(filePath, (exists) => {
-          if (!exists) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<html><body><h1>Error 404: ' + fileUrl + 
-                        ' not found</h1></body></html>');
-            return;
-          }
-          res.statusCode = 200;
-          res.setHeader('Content-Type', 'text/html');
-          fs.createReadStream(filePath).pipe(res);
-        });
-      }
-      else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<html><body><h1>Error 404: ' + fileUrl + 
-                ' not a HTML file</h1></body></html>');
-      }
-    }
-    else {
-        res.statusCode = 404;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('<html><body><h1>Error 404: ' + req.method + 
-                ' not supported</h1></body></html>');
-    }
-  })
+const app = express();
+
+app.use((req, res, next) => {
+  console.log(req.headers);
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/html');
+  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+
+});
+
+const server = http.createServer(app);
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
