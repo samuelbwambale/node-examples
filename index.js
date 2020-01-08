6ea675e113c26bddf52ad5384c8d2f438e89cffe
samuelbwambale <simplesam63@gmail.com>
2020-01-08 00:20:47 -0600

    add promoRouter and leaderRouter - http://localhost:3000/dishes/:dishId - http://localhost:3000/promotions and http://localhost:3000/promotions/:promoId - http://localhost:3000/leaders and http://localhost:3000/leaders/:leaderId

--- index.js
+++ index.js
@@ -58,6 +58,8 @@ const http = require('http');
 const morgan = require('morgan'); // HTTP request logger middleware for node.js
 const bodyParser = require('body-parser');
 const dishRouter = require('./routes/dishRouter');
+const promoRouter = require('./routes/promoRouter');
+const leaderRouter = require('./routes/leaderRouter');
 
 const hostname = 'localhost';
 const port = 3000;
@@ -69,45 +71,25 @@ app.use(bodyParser.json())  // allows us to extract the body of the request mess
                             // the body of the incoming request will be parsed and then added into the 'req' object as req.body. 
                             // So the req.body will give you access to whatever is inside that body of the message
 
+/* req.query, req.params and req.body 
 
-  
-  app.get('/dishes/:dishId', (req,res,next) => {
-      res.end('Will send details of the dish: ' + req.params.dishId +' to you!');
-  });
-  
-  app.post('/dishes/:dishId', (req, res, next) => {
-    res.statusCode = 403;
-    res.end('POST operation not supported on /dishes/'+ req.params.dishId);
-  });
-  
-  app.put('/dishes/:dishId', (req, res, next) => {
-    res.write('Updating the dish: ' + req.params.dishId + '\n');
-    res.end('Will update the dish: ' + req.body.name + 
-          ' with details: ' + req.body.description);
+    req.query comes from query parameters in the URL such as http://foo.com/somePath?name=ted 
+    where req.query.name === "ted".
 
-        /* req.query, req.params and req.body 
+    req.params comes from path segments of the URL that match a parameter in the route definition 
+    such as /song/:songid. So, with a route using that designation and a URL such as /song/48586, 
+    then req.params.songid === "48586".
 
-        req.query comes from query parameters in the URL such as http://foo.com/somePath?name=ted 
-        where req.query.name === "ted".
-
-        req.params comes from path segments of the URL that match a parameter in the route definition 
-        such as /song/:songid. So, with a route using that designation and a URL such as /song/48586, 
-        then req.params.songid === "48586".
-
-        req.body properties come from a form post where the form data (which is submitted in the body contents) 
-        has been parsed into properties of the body tag.
-        */
-  });
-  
-  app.delete('/dishes/:dishId', (req, res, next) => {
-      res.end('Deleting dish: ' + req.params.dishId);
-  });
+    req.body properties come from a form post where the form data (which is submitted in the body contents) 
+    has been parsed into properties of the body tag.
+*/
 
 app.use('/dishes', dishRouter); // mount the dishRouter, any request to /dishes will be handled by dishRouter
+app.use('/promotions', promoRouter);
+app.use('/leaders', leaderRouter);
 app.use(express.static(__dirname + '/public')); // serve static files
 
-// if request for non-existent route, server will serve the default value
-app.use((req, res, next) => {
+app.use((req, res, next) => { // if request for non-existent route, server will serve the default value
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/html');
   res.end('<html><body><h1>This is an Express Server</h1></body></html>');
